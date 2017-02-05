33b8a6de8fe65a807eaf1a133254009f8282b878
Will Langstroth <will@langstroth.com>
2017-02-05 15:12:05 -0500

    Add the trading stuff to secretary

---
+++ oanda.lisp
@@ -0,0 +1,133 @@
+(in-package #:secretary)
+
+;; (require 'drakma)
+;; (require 'st-json)
+(load "secrets")
+
+;; Either you do the following, or you have to (flexi-streams:octets-to-string)
+;; anything that comes from (drakma:http-request)
+(setq drakma:*text-content-types*
+      (cons '("application" . "json")
+            drakma:*text-content-types*))
+
+(defvar *base-url* "https://api-fxtrade.oanda.com/v1/")
+(defvar *account-url*
+  (concatenate 'string *base-url* "accounts/" *default-account*))
+(defvar *trades-url*
+  (concatenate 'string *account-url* "/trades"))
+
+;; *bearer* is in the secrets file
+(defun auth-bearer ()
+  (format nil "Bearer ~a" *bearer*))
+
+(defun price-url-of (instrument)
+  (concatenate 'string *base-url* "prices?instruments=" instrument))
+
+(defun oanda-request (url)
+  (drakma:http-request
+   url
+   :additional-headers `(("Authorization" . ,(auth-bearer)))))
+
+;; Not yet good enough at Lisp to know a way around having to have two nearly
+;; identical functions. st-json:getjso* will only take a literal string.
+(defun ask-price-of (instrument)
+  (st-json:getjso* "ask" (first (prices instrument))))
+
+(defun bid-price-of (instrument)
+  (st-json:getjso* "bid" (first (prices instrument))))
+
+(defun prices (instrument)
+  (st-json:getjso
+   "prices"
+   (st-json:read-json
+    (oanda-request
+     (price-url-of instrument)))))
+
+(defun trades ()
+  (st-json:getjso
+   "trades"
+   (st-json:read-json
+    (oanda-request *trades-url*))))
+
+(defun show-trade (trade-obj)
+  (let*
+      ((name (st-json:getjso* "instrument" trade-obj))
+       (units (st-json:getjso* "units" trade-obj))
+       (trade-price (st-json:getjso* "price" trade-obj))
+       (side (st-json:getjso* "side" trade-obj))
+       (trade-time (subseq (st-json:getjso* "time" trade-obj) 0 19))
+       (price
+        (cond
+          ((string= side "sell") (ask-price-of name))
+          (t (bid-price-of name))))
+       (adjustment-factor
+        (cond
+          ((string= (subseq name 0 3) "USD") price)
+          (t 1)))
+       (side-factor
+        (cond
+          ((string= side "sell") -1)
+          (t 1)))
+       (price-diff (/ (* side-factor
+                         (- (* price units)
+                            (* trade-price units)))
+                      adjustment-factor))
+       (bought-sold
+        (cond ((string= side "sell") "SHORT")
+              (t "LONG")))
+       (pl-string (format nil "~$" price-diff)))
+    (format t "~%~a | ~5a ~5@a ~10a @ ~7@a | P/L: ~7@a ~%"
+            trade-time
+            bought-sold
+            units name
+            trade-price
+            pl-string)))
+
+(defun show-trades ()
+  (loop for trade in (trades)
+       do (show-trade trade)))
+
+(defun account ()
+  (st-json:read-json
+   (oanda-request *account-url*)))
+
+(defun balance ()
+  (st-json:getjso* "balance" (account)))
+
+(defparameter *position-factor* 0.025)
+
+(defun position-size (&optional (size *position-factor*))
+  (* (balance) size))
+
+(defun instrument-url (instrument field)
+  "*default-account* is in the secrets file"
+  (concatenate 'string
+               *base-url*
+               "instruments?"
+               "accountId="
+               *default-account*
+               "&instruments="
+               instrument
+               "&fields="
+               field
+               ))
+
+(defun instrument-data (&optional (instrument "EUR_USD") (field "marginRate"))
+  (st-json:read-json
+   (oanda-request (instrument-url instrument field))))
+
+(defun mid-point (high low)
+  (/ (+ high low) 2))
+
+; For *_USD, side * (position / (current_price - stop))
+; USD_*, above / current_price
+(defun units-for-trade (instrument side stop)
+  (let*
+      ((side-factor
+        (cond ((string= side "sell") -1)
+              (t 1)))
+       (price
+        (cond
+          ((string= side "sell") (ask-price-of instrument))
+          ((string= side "buy") (bid-price-of instrument)))))
+    (format t "~a" (* side-factor (/ (position-size) (- price stop))))))
